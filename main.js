f4fc315e19452833b850fd73d53393eddc888c18
Carlos Gomez <carlosandres0741@gmail.com>
2023-10-01 14:25:40 -0500

    feat: added basic check state

--- main.js
+++ main.js
@@ -26,7 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
 const renderTasks = () => {
   tasksContainer.innerHTML = ''
   tasks.forEach((task, index) => {
-    cardContainer.firstElementChild.childNodes[1].textContent = task
+    cardContainer.firstElementChild.childNodes[1].firstElementChild.textContent = task
+    cardContainer.firstElementChild.childNodes[1].lastElementChild.dataset.taskId = index
     cardContainer.firstElementChild.childNodes[3].firstElementChild.dataset.taskId = index
     cardContainer.firstElementChild.childNodes[3].lastElementChild.dataset.taskId = index
 
@@ -35,12 +36,26 @@ const renderTasks = () => {
 
   const editTaskButtons = document.querySelectorAll('.editTaskButton')
   const deleteTaskButtons = document.querySelectorAll('.deleteTaskButton')
+  const checkTaskButtons = document.querySelectorAll('.checkTaskButton')
 
   deleteTaskButtons.forEach(button => {
     button.addEventListener('click', e => {
       tasks.splice(e.target.dataset.taskId, 1)
       localStorage.setItem(STORAGE_NAME, JSON.stringify(tasks))
       renderTasks()
+    })
+  })
+
+  checkTaskButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.parentElement.firstElementChild.classList.toggle('line-through')
+      button.parentElement.parentElement.classList.toggle('bg-green-600')
+
+      if (button.firstElementChild.classList.contains('ti-check')) {
+        button.firstElementChild.classList.replace('ti-check', 'ti-checks')
+      } else {
+        button.firstElementChild.classList.replace('ti-checks', 'ti-check')
+      }
     })
   })
 
